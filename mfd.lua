055b0ec7235ddb861a648cfc61b385a64a867fdd
Sledmine <40512223+Sledmine@users.noreply.github.com>
2023-03-05 19:48:18 -0600

    use curl impersonate, return proper exit codes

--- mfd.lua
+++ mfd.lua
@@ -2,7 +2,9 @@ local inspect = require "inspect"
 local htmlparser = require "htmlparser"
 local glue = require "glue"
 
-local curlCmdTemplate = "curl -s -L %s"
+-- curl_chrome104 is a wrapper around curl that sets the user agent to Chrome 104
+-- This is needed because the website checks our request and returns a 403 if not a browser
+local curlCmdTemplate = [[curl_chrome104 -s -L %s]]
 local curlCmdDownloadFileTemplate = "curl %s -o %s"
 
 local curlCmdStream = assert(io.popen(curlCmdTemplate:format(arg[1]), "r"))
@@ -18,4 +20,7 @@ for _, e in pairs(elements) do
     local splitUrl = glue.string.split(downloadLink, "/")
     local fileName = splitUrl[#splitUrl]
     os.execute(curlCmdDownloadFileTemplate:format(downloadLink, fileName))
+    os.exit(0)
 end
+print("Error, No download link was found.")
+os.exit(1)
